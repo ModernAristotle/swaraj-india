82021b07ff8b6e4b1a1da56b03dd0fdf37c14d5a
Ravi Sharma <ping2ravi@gmail.com>
2016-10-15 23:57:49 +0100

    Fix for JPA annotations

--- admin/src/main/java/com/aristotle/admin/App.java
+++ admin/src/main/java/com/aristotle/admin/App.java
@@ -7,16 +7,20 @@ import com.next.dynamo.context.DynamoServiceContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.Arrays;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.aristotle.admin"})
 @EnableAutoConfiguration
+@EnableJpaRepositories(basePackages = {"com.aristotle.core.persistance.repo"})
+@EntityScan(basePackages = {"com.aristotle.core.persistance"})
 //@Import({SwaggerConfig.class, CoreConfig.class, DatabaseConfig.class, DynamoServiceContext.class})
 public class App extends SpringBootServletInitializer {
 
