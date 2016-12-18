b082e8d818743fd7115cae50d7cc73b75aee6e7f
Ravi Sharma <ping2ravi@gmail.com>
2016-12-18 23:46:00 +0000

    Fixed Content Sorting field

--- core/src/main/java/com/aristotle/core/persistance/repo/ContentRepository.java
+++ core/src/main/java/com/aristotle/core/persistance/repo/ContentRepository.java
@@ -12,25 +12,25 @@ import java.util.Set;
 public interface ContentRepository extends JpaRepository<Content, Long> {
 
     //Web
-    @Query("select content from Content content where content.contentType=?1 and content.global=true order by dateCreated desc")
+    @Query("select content from Content content where content.contentType=?1 and content.global=true order by publishDate desc")
     public List<Content> getGlobalContent(String contentType, Pageable pageable);
 
-    @Query("select content from Content content join content.locations locations where locations.id in ?2 and content.contentStatus='Published' and content.contentType=?1 order by content.dateCreated desc")
+    @Query("select content from Content content join content.locations locations where locations.id in ?2 and content.contentStatus='Published' and content.contentType=?1 order by content.publishDate desc")
     public List<Content> getLocationPublishedContent(String contentType, Set<Long> locationIds, Pageable pageable);
 
     @Query("select count(content) from Content content join content.locations locations where content.contentType=?1 and locations.id in ?2 and content.contentStatus='Published'")
     public long getLocationPublishedContentCount(ContentType contentType, Set<Long> locationIds);
 
-    @Query("select content from Content content where content.contentType=?1 and content.global=true and content.contentStatus='Published' order by content.dateCreated desc")
+    @Query("select content from Content content where content.contentType=?1 and content.global=true and content.contentStatus='Published' order by content.publishDate desc")
     public List<Content> getGlobalPublishedContent(ContentType contentType, Pageable pageable);
 
     @Query("select count(content) from Content content where content.contentType=?1 and content.global=true  and content.contentStatus='Published'")
     public long getGlobalPublishedContentCount(ContentType contentType);
 
-    @Query("select content from Content content join content.locations locations where locations.id in ?1 and content.contentStatus='Published' and content.contentType in ('News', 'PressRelease') order by content.dateCreated desc")
+    @Query("select content from Content content join content.locations locations where locations.id in ?1 and content.contentStatus='Published' and content.contentType in ('News', 'PressRelease') order by content.publishDate desc")
     public List<Content> getLatestLocationPublishedContent(Set<Long> locationIds, Pageable pageable);
 
-    @Query("select content from Content content where content.contentStatus='Published' and content.contentType in ('News', 'PressRelease') order by content.dateCreated desc")
+    @Query("select content from Content content where content.contentStatus='Published' and content.contentType in ('News', 'PressRelease') order by content.publishDate desc")
     public List<Content> getLatestGlobalPublishedContent(Pageable pageable);
 
     //Admin
