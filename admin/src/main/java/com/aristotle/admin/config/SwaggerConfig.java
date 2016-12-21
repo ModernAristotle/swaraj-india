5ce671e0fdcbcd22ec6a39b847189865d14a1dd2
Ravi Sharma <ping2ravi@gmail.com>
2016-12-21 03:02:07 +0000

    Updated API Info

--- admin/src/main/java/com/aristotle/admin/config/SwaggerConfig.java
+++ admin/src/main/java/com/aristotle/admin/config/SwaggerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -24,7 +25,8 @@ public class SwaggerConfig {
 
 
     private ApiInfo apiInfo() {
-        ApiInfo apiInfo = new ApiInfo("My REST API", "Some custom description of API.", "API TOS", "Terms of service", "ping2ravi@gmail.com", "License of API", "API license URL");
+        Contact contact = new Contact("Ravi Sharma", "", "ping2ravi@gmail.com");
+        ApiInfo apiInfo = new ApiInfo("My REST API", "Some custom description of API.", "API TOS", "Terms of service", contact, "License of API", "API license URL");
         return apiInfo;
     }
 }
