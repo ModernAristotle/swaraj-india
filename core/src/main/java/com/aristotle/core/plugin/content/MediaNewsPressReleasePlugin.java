bcf15619f02eb4a72a62523d1c4e8513a7ab3eb2
Ravi Sharma <ping2ravi@gmail.com>
2016-12-23 08:07:36 +0000

    Fixed plugin hierarchy

--- core/src/main/java/com/aristotle/core/plugin/content/MediaNewsPressReleasePlugin.java
+++ core/src/main/java/com/aristotle/core/plugin/content/MediaNewsPressReleasePlugin.java
@@ -1,7 +1,5 @@
 package com.aristotle.core.plugin.content;
 
-import com.aristotle.core.service.ContentService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
@@ -12,12 +10,7 @@ import javax.servlet.http.HttpServletRequest;
  * Created by ravi on 04/12/16.
  */
 @Component
-public class MediaNewsPressReleasePlugin extends MediaNewsPressReleaseListPlugin {
-
-    @Autowired
-    private ContentService contentService;
-    @Autowired
-    private ContentFieldSetting contentFieldSetting;
+public class MediaNewsPressReleasePlugin extends AbstractMediaNewsPressReleaseListPlugin {
 
     private final Pageable firstPage = new PageRequest(0, 3);
 
